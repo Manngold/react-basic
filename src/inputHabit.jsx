6a0ad33485b9d3aed9c969896c4d802486032355
Manngold <jsw4820@gmail.com>
2020-09-25 15:08:51 +0900

    fix input component

--- src/inputHabit.jsx
+++ src/inputHabit.jsx
@@ -1,22 +1,18 @@
 import React, { Component } from 'react';
 
 class InputHabit extends Component {
-  handleAdd = () => {
-    const name = document.querySelector('.habit_input').value;
-    if (name.length > 0) {
-      this.props.onAdd(name);
-    } else return;
-    this.clearInput();
-  };
-  clearInput = () => {
-    document.querySelector('.habit_input').value = '';
+  textInput = React.createRef();
+  onSubmit = (e) => {
+    e.preventDefault();
+    this.props.onAdd(this.textInput.current.value);
+    this.textInput.current.value = '';
   };
   render() {
     return (
-      <div>
-        <input className='habit_input' type='text' />
-        <button onClick={this.handleAdd}>Add</button>
-      </div>
+      <form className='add-form' onSubmit={this.onSubmit}>
+        <input className='habit_input' type='text' ref={this.textInput} />
+        <button>Add</button>
+      </form>
     );
   }
 }
